cac0670d0fb88da18710569268441fb95ee4bfb1
CelticSlash <37378603+CelticSlash@users.noreply.github.com>
2021-02-28 20:01:28 -0300

    Aula 1

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Funcionario extends Model
 {
     use HasFactory;
+
+    protected $fillable = [ 'id', 'nome', 'endereco', 'email', 'telefone'];
+
+    protected $table = 'Funcionario';
+    
+    /*  É possivel alterar a chave primária usando:
+            protected $primaryKey = 'nome da pk'
+        
+        Caso não queira auto_increment:
+            public $increment = false;
+        
+        Para definir tipo:
+            protected $keyType = 'string';
+        
+        Para remover os campos timestamp:
+            public $timestamps = false;
+    */
 }

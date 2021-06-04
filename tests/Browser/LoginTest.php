a20b52ed803106ccddf8ac148d5468c6cacfc604
CelticSlash <henriquepereiralima@yahoo.com.br>
2021-06-04 19:46:42 -0300

    Aula 04/06

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laracasts');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Posto na frente do Senac');
+        });
+    }
+}
